3608208571396fed5c1ccb2cb1d00f01c5be39f1
Pradnesh <pradneshnaik14@gmail.com>
2023-09-18 17:09:13 -0700

    First commit-Pradnesh

---
+++ Assets/Scripts/ObjectDeletion.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class ObjectDeletion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject wallObject;
+    private List<GameObject> keepTrack= new List<GameObject>();
+    
+    void Start()
+    {
+        foreach(Transform child in wallObject.transform)
+        {
+            keepTrack.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        try
+        {
+            int number = Random.Range(0, keepTrack.Count);
+            if (col.gameObject.name == "Player")
+            {
+                Destroy(keepTrack[number]);
+                keepTrack.RemoveAt(number);
+            }
+        }
+        catch(Exception ex)
+        {
+            print("Error");
+        }
+    }
+}
