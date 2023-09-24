25636c3c1ca97e8502cc13d6e8ed6d9c80541256
Rohan Thomas <rohangeorgethomas@gmail.com>
2023-09-23 17:58:47 -0700

    Added game over text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,23 +8,30 @@ public class GameManager : MonoBehaviour
     bool gameRunning = true;
     public string startText = "";
     public FloatingTextModifier playerText;
+    public GameObject gameOverText;
     public void GameOver()
     {
         if(gameRunning)
         {
             gameRunning = false;
-            Debug.Log("Game Over");
-            Invoke("Restart", 1);
+            gameOverText.SetActive(true);
+            Invoke("Restart", 3);
         }
     }
 
     private void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOverText.SetActive(false);
     }
 
     private void Start()
     {
+        if (gameOverText)
+        {
+            gameOverText.SetActive(false);
+        }
+        
         if(playerText)
         {
             playerText.ChangeText(startText);
